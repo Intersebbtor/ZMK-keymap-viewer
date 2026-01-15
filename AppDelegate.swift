991c5a5f48b2dd7743ab30654c9c5c7265fda346
Intersebbtor <44674833+Intersebbtor@users.noreply.github.com>
2026-01-15 10:39:00 +0100

    Create AppDelegate.swift for macOS menu bar setup and app lifecycle

---
+++ AppDelegate.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the menu bar application interface here
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
